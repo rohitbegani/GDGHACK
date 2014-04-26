fe1a51ffe0a4f654e03c5720b8fb44dc495bf642
saasbook <saasbook@saasbook.(none)>
2014-04-27 00:58:23 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gdghack::Application.config.secret_token = '5be532bac4b2e317d98e4ba4f75ff2570ff465853a4ba99e0b88cddf25128d7fbe985526b9c39f81be0197253428cccb477b57d2837335457d955e7c655404f8'
