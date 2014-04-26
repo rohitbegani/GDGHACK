bff20777bf8ef4def55fe8835d039e9c11300b25
saasbook <saasbook@saasbook.(none)>
2014-04-27 04:06:16 +0530

    Added the map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,12 @@
 class MapController < ApplicationController
 
 	def index
-		
+	    @weddings = Wedding.all
+		  @hash = Gmaps4rails.build_markers(@weddings) do |wedding, marker|
+			  marker.lat wedding.latitude
+			  marker.lng wedding.longitude
+			  marker.infowindow wedding.description
+		end
 	end
 	
 end
--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -3,6 +3,7 @@ class WeddingsController < ApplicationController
   # GET /weddings.json
   def index
     @weddings = Wedding.all
+  
 
     respond_to do |format|
       format.html # index.html.erb
